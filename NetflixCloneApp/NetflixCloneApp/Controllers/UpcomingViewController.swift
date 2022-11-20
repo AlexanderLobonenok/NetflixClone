61a7ee7fc580d526d43d330815e6ce50d5f40a65
AlexanderLobonenok <110338796+AlexanderLobonenok@users.noreply.github.com>
2022-11-20 23:48:00 +0300

    Progress 1

---
+++ NetflixCloneApp/NetflixCloneApp/Controllers/UpcomingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewController.swift
+//  NetflixCloneApp
+//
+//  Created by Alexander Lobonenok on 20.11.22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+}
